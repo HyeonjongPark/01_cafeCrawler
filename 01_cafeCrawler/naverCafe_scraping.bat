be751efe163c32e48e7c5eef34da5b6fec34499d
HyeonjongPark <guswhd9844@gmail.com>
2021-05-27 10:34:41 +0900

    batch file

--- 01_cafeCrawler/naverCafe_scraping.bat
+++ 01_cafeCrawler/naverCafe_scraping.bat
@@ -8,6 +8,16 @@ python naverCafeReplyAll.py
 cd D:\workspace\010_crawler_naverCafe2
 
 Rscript src/03_preprocessing.R
+
+git add data/naverCafeReply_A.xlsx
+git add data/naverCafeWrite_A.xlsx
+git commit -m "collected data"
+
+git add out/all_frame.csv
+git commit -m "publishing data"
+
+git push
+
 Rscript dash_executor.R
 
 exit
